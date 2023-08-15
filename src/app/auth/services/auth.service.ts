abbb933faa3ffe2b5116f863a99599be421a4ca8
HeiderHDev <heiderhrnndz@gmail.com>
2023-08-14 23:16:48 -0500

    isNotAuthenticated Guard agregado

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -20,7 +20,17 @@ export class AuthService {
   public authStatus = computed(() => this._authStatus() );
 
 
-  constructor() { }
+  constructor() {
+    this.checkAuthStatus().subscribe();
+   }
+
+  private setAuthentication(user: User, token: string): boolean{
+    this._currentUser.set( user );
+    this._authStatus.set( AuthStatus.authenticated );
+    localStorage.setItem( 'token', token );
+
+    return true;
+  }
 
   login( email: string, password: string ): Observable<boolean> {
 
@@ -29,19 +39,9 @@ export class AuthService {
 
     return this.http.post<LoginResponse>( url, body )
       .pipe(
-        tap(({user, token})=>{
-          this._currentUser.set( user );
-          this._authStatus.set( AuthStatus.authenticated );
-          localStorage.setItem( 'token', token );
-          // console.log({user, token});
-        }),
-        map( ()=> true ),
-
-        // TODO: errores
+        map(({user, token})=> this.setAuthentication( user, token )),
         catchError( err => throwError(()=>err.error.message))
       );
-
-    return of( true );
   }
 
   checkAuthStatus(): Observable<boolean> {
@@ -57,21 +57,13 @@ export class AuthService {
 
       return this.http.get<ChekTokenResponse>( url, { headers })
         .pipe(
-          map( ({token, user})=> {
-            this._currentUser.set( user );
-            this._authStatus.set( AuthStatus.authenticated );
-            localStorage.setItem( 'token', token );
-            
-            return true;
-          }),
+          map(({user, token})=> this.setAuthentication( user, token )),
           // Error
           catchError(()=> {
             this._authStatus.set( AuthStatus.notAuthenticated );
             return of( false );
           })
         )
-
-    
   }
 
 }
